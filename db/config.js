6267f6c802d514ad04da523b5ee3442f06267436
Mauro Villagra <maurovillagra257@gmail.com>
2024-09-15 22:34:09 -0300

    Probe la conexion a mongo

--- db/config.js
+++ db/config.js
@@ -1,14 +1,14 @@
-// require('dotenv').config(); // Asegúrate de que esto esté al principio del archivo
-// const mongoose = require('mongoose');
+require('dotenv').config(); // Asegúrate de que esto esté al principio del archivo
+const mongoose = require('mongoose');
 
-// const mongoURI = process.env.MONGODB; // Obtén la URI de las variables de entorno
+const mongoURI = process.env.MONGODB; // Obtén la URI de las variables de entorno
 
-// if (!mongoURI) {
-//   throw new Error('MONGODB URI no está definida en las variables de entorno');
-// }
+if (!mongoURI) {
+  throw new Error('MONGODB URI no está definida en las variables de entorno');
+}
 
-// mongoose.connect(mongoURI)
-//   .then(() => console.log('db conectada'))
-//   .catch((error) => console.error('Error al conectar a MongoDB:', error));
+mongoose.connect(mongoURI)
+  .then(() => console.log('db conectada'))
+  .catch((error) => console.error('Error al conectar a MongoDB:', error));
 
-// module.exports = mongoose;
+module.exports = mongoose;
