f45eebd42557b2a5bd7a1b504f7e4523fc3886a9
Mauro Villagra <maurovillagra257@gmail.com>
2024-09-15 22:35:07 -0300

    añadi las rutas de canchas

--- routes/cancha.routes.js
+++ routes/cancha.routes.js
@@ -1,3 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const {
+  obtenerCanchas,
+  obtenerCancha,
+  crearCancha,
+  actualizarCancha,
+  borrarCancha
+} = require("../services/cancha.services.js");
 
+router.get("/", async (req, res) => {
+  const result = await obtenerCanchas();
+  res.status(result.statusCode).json(result);
+});
+
+router.get("/:idCancha", async (req, res) => {
+  const result = await obtenerCancha(req.params.idCancha);
+  res.status(result.statusCode).json(result);
+});
+
+router.post("/", async (req, res) => {
+  const result = await crearCancha(req.body);
+  res.status(result.statusCode).json(result);
+});
+
+router.put("/:idCancha", async (req, res) => {
+  const result = await actualizarCancha(req.body, req.params.idCancha);
+  res.status(result.statusCode).json(result);
+});
+
+router.delete("/:idCancha", async (req, res) => {
+  const result = await borrarCancha(req.params.idCancha);
+  res.status(result.statusCode).json(result);
+});
+
+module.exports = router;
