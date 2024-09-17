648765ca3d64c823aaea4bc595c7d3bb8ef7b3e6
Mauro Villagra <maurovillagra257@gmail.com>
2024-09-16 23:38:47 -0300

    añadi los servicios de reserva

---
+++ services/reserva.services.js
@@ -0,0 +1,74 @@
+const ReservaModels = require("../models/reservaSchema.js");
+
+const obtenerReservas = async () => {
+    const reservas = await ReservaModels.find();
+    return {
+        reservas,
+      statusCode: 200,
+    };
+  };
+  
+  const obtenerReserva = async (idReserva) => {
+    const reserva = await CanchaModel.findOne({ _id: idReserva });
+    return {
+        reserva,
+      statusCode: 200,
+    };
+  };
+  
+  const crearReserva = async (body) => {
+   
+    
+    try {
+      
+      const reservaFechaExistente = await ReservaModels.findOne({ FechaHora: body.FechaHora });
+    const reservaCanchaExistente = await ReservaModels.findOne({ ID_Cancha: body.ID_Cancha });
+
+      if (reservaFechaExistente || reservaCanchaExistente) {
+        
+              return {
+                  msg: "La cancha ya esta reservada en ese horario",
+                  statusCode: 400,
+                };
+          
+        
+        }
+      const nuevaReserva = new ReservaModels(body);
+      await nuevaReserva.save();
+  
+      return {
+        msg: "Reserva creada con éxito",
+        statusCode: 201,
+      };
+    } catch (error) {
+      return {
+        msg: "Error al crear la reserva",
+        statusCode: 500,
+        error: error.message,
+      };
+    }
+  };
+  
+  
+  const actualizarReserva = async (body, idReserva) => {
+    await ReservaModels.findByIdAndUpdate({ _id: idReserva }, body);
+    const reserva = await ReservaModels.find();
+    return {
+      msg: "Reserva actualizada",
+      reserva,
+      statusCode: 200,
+    };
+  };
+  
+  const borrarReserva = async (idReserva) => {
+    await ReservaModels.findByIdAndDelete({ _id: idReserva });
+  
+    return {
+      msg: "Reserva eliminada",
+      statusCode: 200,
+    };
+  };
+  
+  module.exports={
+    actualizarReserva, borrarReserva, crearReserva, obtenerReserva, obtenerReservas
+  }
