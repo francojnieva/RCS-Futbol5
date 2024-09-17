8f8d0e60f2f68e500ac211768f40b8f09b185a86
francojnieva <franco97nieva@hotmail.com>
2024-09-17 17:51:16 -0300

    add reservation confirmation

--- templates/reservationConfirmation.js
+++ templates/reservationConfirmation.js
@@ -1,6 +1,6 @@
 const transporter = require('../helpers/nodemailer')
 
-const reservationConfirmation = async (email) => {
+const reservationConfirmation = async (email, date, courtNumber) => {
     try {
         const info = await transporter.sendMail({
             from: `"RCS-Fútbol5" <${process.env.GMAIL_USER_NODEMAILER}>`,
@@ -10,8 +10,8 @@ const reservationConfirmation = async (email) => {
             <main style="text-align: center; font-family: 'Poppins', sans-serif;">
                 <div style="padding: 2rem 0; background-color: #45B649;"></div>
                 <h1 style="font-family: 'Poppins', sans-serif;">¡Tu reserva en RCS-Fútbol5! ⚽</h1>
-                <p style="font-family: 'Poppins', sans-serif, font-weight: 600;">N° de cancha: </p>
-                <p style="font-family: 'Poppins', sans-serif, font-weight: 600;">Fecha y hora: </p>
+                <p style="font-family: 'Poppins', sans-serif, font-weight: 600;">N° de cancha: ${courtNumber}</p>
+                <p style="font-family: 'Poppins', sans-serif, font-weight: 600;">Fecha y hora: ${date}</p>
                 <p style="font-family: 'Poppins', sans-serif;">Saludos<br/>El equipo de RCS-Fútbol5 ⚽😉</p>
                 <div style="padding: 2rem 0; background-color: #45B649;"></div>
             </main>`,
