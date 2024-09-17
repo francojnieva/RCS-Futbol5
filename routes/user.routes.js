85bb1de0ff091f22b8c787ca7d61dd5ac81cb43f
Mauro Villagra <maurovillagra257@gmail.com>
2024-09-16 23:39:00 -0300

    añadi las rutas de usuario

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,26 +1,26 @@
 const { Router } = require("express");
 const {actualizarUnUsuario, inicioDeSesionUsuario, crearUsuario, obtenerTodosLosUsuarios, obtenerUnUsuario, registrarUsuario} = require("../controllers/users.controllers.js")
 const { check } = require("express-validator");
-// const auth = require("../middlewares/auth");
+const auth = require("../middlewares/auth.js");
 const router = Router();
-router.get("/", obtenerTodosLosUsuarios);
-router.get("/:idUsuario", obtenerUnUsuario);
+router.get("/", auth("admin"), obtenerTodosLosUsuarios);
+router.get("/:idUsuario", auth("admin"), obtenerUnUsuario);
 
 router.post(
-  "/",
+  "/CrearUsuario",
   [
     check("nombreUsuario", "Campo NOMBREUSUARIO esta vacio").not().isEmpty(),
     check("contrasenia", "Campo CONTRSAENIA esta vacio").not().isEmpty(),
     check("contrasenia", "Min: 8 caracteres y Max: 30 caracteres").isLength({
       min: 8,
       max: 30,
-    }),
+    }), auth("admin"),
   ],
   crearUsuario
 );
 router.post("/RegistrarUsuario", registrarUsuario);
 
-router.post("/iniciarSesion", inicioDeSesionUsuario);
-router.put("/:idUsuario", actualizarUnUsuario);
+router.post("/IniciarSesion", inicioDeSesionUsuario);
+router.put("/:idUsuario", auth("user"), actualizarUnUsuario);
 
 module.exports = router;
